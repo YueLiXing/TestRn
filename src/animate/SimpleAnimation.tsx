3519a5b044facf7afd99902bd0a6609bcd15f957
岳立兴 <yuelixing@linkdora.com>
2025-02-15 09:36:59 +0800

    feat 格式化代码

--- src/animate/SimpleAnimation.tsx
+++ src/animate/SimpleAnimation.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { Animated, View, Text, StyleSheet, Button } from 'react-native';
 
 const SimpleAnimation = () => {
